a838aacaba2a21c9ec1ece372402e89f38254090
Lamia Rizk <lamiarizk@gmx.de>
2025-03-17 13:21:15 +0100

    first commit

---
+++ src/components/quotebox/Quotebox.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import "./Quotebox.css";
+
+const Quotebox = () => {
+  const [quote, setQuote] = useState<string>("");
+
+  // Neue Funktion für das Abrufen eines Zitats (Arrow Function)
+  const fetchNewQuote = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => setQuote(data.slip.advice))
+      .catch((error) =>
+        console.error("Fehler beim Abrufen des Zitats:", error)
+      );
+  };
+
+  // Beim ersten Rendern initiales Zitat laden
+  useEffect(() => {
+    fetchNewQuote();
+  }, []);
+
+  return (
+    <div className="quotebox">
+      <h1>Quote of the Day</h1>
+      <p>{quote}</p>
+      {/* Pattern Divider */}
+      <img
+        src="/images/pattern-divider-mobile.svg"
+        alt="Pattern Divider"
+        className="pattern-divider"
+      />
+      {/* Würfel-Icon mit Kreis */}
+      <div className="dice-icon-wrapper" onClick={fetchNewQuote}>
+        <img
+          src="/images/icon-dice.svg"
+          alt="Dice icon"
+          className="dice-icon"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Quotebox;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Quotebox from "../components/quotebox/Quotebox";
+
+const Home = () => {
+  return (
+    <>
+      <Quotebox />
+    </>
+  );
+};
+
+export default Home;
